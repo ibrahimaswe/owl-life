c23571b93c00f88f3b7eb5722d97052e7c001378
ibrahimaswe <ibrahimaswe@gmail.com>
2025-02-22 02:52:22 -0500

    init

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -29,7 +29,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Owl Points',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
@@ -50,7 +50,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="two"
         options={{
-          title: 'Tab Two',
+          title: 'Chat',
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
         }}
       />
